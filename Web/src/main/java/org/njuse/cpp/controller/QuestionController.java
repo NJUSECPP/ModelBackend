ad35a3c4d723d493b62a3dd583b94d037df287e6
rose_cat707 <rose_cat707@outlook.com>
2023-11-22 23:31:29 +0800

    优化接口

--- Web/src/main/java/org/njuse/cpp/controller/QuestionController.java
+++ Web/src/main/java/org/njuse/cpp/controller/QuestionController.java
@@ -33,11 +33,9 @@ public class QuestionController {
     }
 
     @GetMapping("/getAll")//分页查询所有题目
-    public QuestionResponseDTO getQuestions(
-            @RequestBody Map<String, String> requestBody) {
-        Integer pageSize = Integer.parseInt(requestBody.get("pageSize"));
-        Integer pageNo = Integer.parseInt(requestBody.get("pageNo"));
-        String keyword = requestBody.get("keyword");
+    public QuestionResponseDTO getQuestions(@RequestParam("pageSize") Integer pageSize,
+            @RequestParam("pageNo") Integer pageNo,
+            @RequestParam("keyword") String keyword) {
         List<QuestionBO> questions = questionService.getQuestionsByPage(pageSize, pageNo, keyword);
         int total = questionService.countQuestions(keyword);
 
@@ -52,7 +50,7 @@ public class QuestionController {
         return questionService.addQuestion(name, description);
     }
 
-    @PutMapping("/update")
+    @PostMapping("/update")
     public QuestionBO updateQuestion(
             @RequestBody Map<String, String> requestBody) {
         String id = requestBody.get("id");
@@ -63,10 +61,9 @@ public class QuestionController {
 
     @GetMapping("/testcase/getAll")//分页查询所有用例
     public TestcaseResponseDTO getAllTestcases(
-            @RequestBody Map<String, Integer> requestParams) {
-        Integer pageSize = requestParams.get("pageSize");
-        Integer pageNo = requestParams.get("pageNo");
-        Integer questionId = requestParams.get("questionId");
+            @RequestParam("pageSize") Integer pageSize,
+            @RequestParam("pageNo") Integer pageNo,
+            @RequestParam("questionId") Integer questionId) {
 
         // 调用 service 方法获取数据
         List<TestcasePO> testcaseList = testcaseService.getAllTestcases(pageSize, pageNo, questionId);
@@ -84,7 +81,7 @@ public class QuestionController {
         return questionBO;
     }
 
-    @DeleteMapping("/testcase/batchDelete")
+    @PostMapping("/testcase/batchDelete")
     public QuestionBO batchDeleteTestcases(@RequestBody Map<String, Integer[]> requestBody) {
         // 调用 service 方法批量删除测试用例，并返回 questionId
         Integer[] testcaseIds = requestBody.get("testcaseIds");
@@ -99,7 +96,7 @@ public class QuestionController {
         return questionBO;
     }
 
-    @DeleteMapping("/delete")
+    @PostMapping("/delete")
     public ResponseEntity<Object> deleteQuestionAndTestcases(@RequestBody Map<String, Integer> payload) {
         // 调用 service 方法删除题目及其所有用例
         Integer questionId = payload.get("questionId");
